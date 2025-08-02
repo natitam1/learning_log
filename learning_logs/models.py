6d173ad236507f93022efc9441d7f351e4a34926
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-08-02 13:01:48 +0300

    Register entry with the admin site

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -20,4 +20,4 @@ class Entry(models.Model):
 
     def __str__(self):
         """Return a simple string representing the entry."""
-        return f"{self.text[:50]}"
+        return f"{self.text[:50]}..."
