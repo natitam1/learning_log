fd00c403c41a6eefd34216c454d972f402b7dd2a
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-08-05 14:58:59 +0300

    Fix the bug of double quote

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     # Page that shows all topics.
     path('topics/', views.topics, name='topics'),
     # Detail page for a single topic.
-    path('topics/<int:topic_id/', views.topic, name="topic"),
+    path('topics/<int:topic_id>/', views.topic, name="topic"),
 ]
